c0c2f707cc273e50053b31fb37c380e51283c072
Rick Heist <rick.heist@gmail.com>
2023-04-19 13:47:54 -0400

    got api call working

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
 const searchButton = document.getElementById('searchbutton');
 const searchCity = document.getElementById('search');
-let todaysForecast = document.getElementById('todaysForecast');
+let todaysForecast = document.getElementById('todaysforecast');
 let fiveDayForecast = document.getElementById('card-deck');
 
 function getTodaysWeather() {
-    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=e543b07a6edfe782636b4c5e7cece914'; //api isnt being called
+    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=e543b07a6edfe782636b4c5e7cece914&units=imperial'; 
     
-    fetch(currentWeather)
+   fetch(currentWeather)
         .then(function (response){
-            console.log(response);
             return response.json();
         })
         .then(function (data) {
-            for (var i = 0; i < data.length; i++) {
+            for (var i = 0; i < data.weather.length; i++) {
                 var createWeatherToday = document.createElement('li');
-                createWeatherToday.textContent = data[i].main;// can multiple objects be pulled before being appended?
+                createWeatherToday.textContent = data.weather[i].description;
+                // can multiple objects be pulled before being appended?
+                todaysForecast.innerHTML = "Todays Forecast",
                 todaysForecast.appendChild(createWeatherToday);
             }
         })
 }
 
 searchButton.addEventListener('click', getTodaysWeather);//both today and five day functions need to be called on button click
-console.log(getTodaysWeather);
 
 
 function getFiveDay() {
-    var weeksWeather = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=philadelphia&cnt=5&appid=e543b07a6edfe782636b4c5e7cece914';
+    var weeksWeather = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=philadelphia&cnt=5&appid=e543b07a6edfe782636b4c5e7cece914&units=imperial';
 
     fetch(weeksWeather)
         .then(function(response){
