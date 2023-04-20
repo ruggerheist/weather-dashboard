98f4e73cdab6813e3a232b8442e8c5d14ec3c00d
Rick Heist <rick.heist@gmail.com>
2023-04-20 00:36:23 -0400

    5 day forecast api is pulling. errors within daily forecast

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,53 @@
 const searchButton = document.getElementById('searchbutton');
-const searchCity = document.getElementById('search');
 let todaysForecast = document.getElementById('todaysforecast');
 let fiveDayForecast = document.getElementById('card-deck');
 
-function getTodaysWeather() {
-    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=e543b07a6edfe782636b4c5e7cece914&units=imperial'; 
+
+function getTodaysWeather(event) {
+    event.preventDefault();
+    const searchCity = document.getElementById('search').value;
+    console.log(searchCity);
+    var currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=e543b07a6edfe782636b4c5e7cece914&units=imperial`; 
     
    fetch(currentWeather)
         .then(function (response){
             return response.json();
         })
         .then(function (data) {
-            for (var i = 0; i < data.weather.length; i++) {
-                var createWeatherToday = document.createElement('li');
-                createWeatherToday.textContent = data.weather[i].description;
-                // can multiple objects be pulled before being appended?
-                todaysForecast.innerHTML = "Todays Forecast",
-                todaysForecast.appendChild(createWeatherToday);
+            console.log(data);
+            var longitude  = data.coord.lon;
+            var latitude = data.coord.lat;
+            console.log(longitude, latitude);
+            for (var i = 0; i < data.weather.length; i++) { //delete for loop
+                var weatherDescription = document.createElement('li');
+                weatherDescription.textContent = data.weather[i].description;
+                // create multiple li elements
+                todaysForecast.innerHTML = "Todays Forecast";
+                todaysForecast.appendChild(weatherDescription);
+                var tempHigh = document.createElement('li');
+                tempHigh.textContent = list.main[i].temp_min; //causing uncaught (in promise) error
+                todaysForecast.appendChild(tempHigh);
             }
+            getFiveDay(longitude, latitude);
         })
+        
 }
 
-searchButton.addEventListener('click', getTodaysWeather);//both today and five day functions need to be called on button click
+searchButton.addEventListener('click', getTodaysWeather);
 
-
-function getFiveDay() {
-    var weeksWeather = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=philadelphia&cnt=5&appid=e543b07a6edfe782636b4c5e7cece914&units=imperial';
-
+function getFiveDay(longitude, latitude) {
+    var weeksWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=e543b07a6edfe782636b4c5e7cece914&units=imperial`;
     fetch(weeksWeather)
         .then(function(response){
             return response.json();
         })
         .then(function (data){
-            for (var i = 0; i < data.length; i++) {
+            console.log(data);
+            //for (var i = 0; i < data.length; i++) {
                 //var createFiveDay = // stopped here, create ids for each row per card?
-            }
+            //}
         })
 }
 
-// TO DO: get user search input to append {CREATE NAME} in both urls
-// get API call working
+// TO DO:
 // get localstorage working and populating recent searches without reset on page load
